fe05bb7016830ac16a68d3e37dc4d30520d3b8f2
brayan <126084472+bryan005-d@users.noreply.github.com>
2024-10-31 18:17:48 +0000

    todo php

--- login.php
+++ login.php
@@ -1,31 +1,127 @@
 <?php
-include 'conexion.php';
+    include("conexion.php");
+    session_start();
+    $error='';
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+      // username and password sent from form
+    $myusername = mysqli_real_escape_string($db,$_POST['username']);
+    $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $correo = $_POST['correo'];
-    $contrasena = $_POST['contrasena'];
+      $sql = "SELECT * FROM users WHERE email = '$myusername' and password_hash = '$mypassword'";
 
-    // Buscar el usuario en la base de datos
-    $sql = "SELECT * FROM users WHERE correo = '$correo'";
-    $result = $conn->query($sql);
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_num_rows($result);
+    $count = mysqli_num_rows($result);
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        
-        // Verificar la contraseña
-        if (password_verify($contrasena, $row['contrasena'])) {
-            session_start();
-            $_SESSION['usuario'] = $row['nombre_usuario'];
-            echo "Inicio de sesión exitoso";
-            header("Location: pagina_inicio.php"); // Redirigir a una página segura
-            exit();
-        } else {
-            echo "Contraseña incorrecta";
-        }
+    if($count == 1) {
+	
+         // session_register("myusername");
+        $_SESSION['login_user'] = $myusername;
+        header("location: welcome.php");
     } else {
-        echo "Usuario no encontrado";
+        $error = "Your Login Name or Password is invalid";
     }
-}
-
-$conn->close();
+    }
 ?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="login.css">
+    
+</head>
+<body>
+    <header>
+        <h3 class="logo">VISION CREATIVA</h3>
+        <nav class="navigation">
+            <a href="#">explorar</a>
+            <a href="#">servicios</a>
+            <a href="#">contactanos</a>
+            <button class="btninicio">
+                <a href="index.html">inicio</a>
+            </button>
+            <button class="btnfolder">
+                <a href="folder.html">portafolio</a>
+            </button>
+        </nav>
+    </header>
+    <div class="wraperlogin">
+        <div class="form-box login">
+
+            <form action = "" method = "post">
+
+                <div class="input-box">
+                    <span class="icono">
+                        <ion-icon name="mail-open"></ion-icon>
+                    </span>
+                    <input type="email" name = "username" required>
+                    <label>correo</label>
+                </div>
+                <div class="input-box">
+                    <span class="icono">
+                        <ion-icon name="lock-closed"></ion-icon>
+                    </span>
+                    <input type="password"name = "password" required>
+                    <label>contraseña</label>
+                </div>
+                <div class="remember_forgot">
+                    <label>recordar </label>
+                        <a href="#">mi contraseña?</a>
+                        <input type = "submit" value = " Submit "/>acceso <br />
+                    <div class="login-register">
+                        <p>no tengo cuenta
+                            <a href="#"class="register-Link">register</a>
+                        </p>
+                    </div>
+                </div>
+            </form>
+            <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+
+        </div>
+    </div>
+    <div class="wraperregister">
+        <div class="form-box register">
+            <h2>registrarse</h2>
+            <form action="#">
+                <div class="input-box">
+                    <span class="icono">
+                        <ion-icon name="person-circle-outline"></ion-icon>
+                    </span>
+                    <input type="text"required>
+                    <label>nombre del usuario</label>
+                </div>
+                <div class="input-box">
+                    <span class="icono">
+                        <ion-icon name="mail-open"></ion-icon>
+                    </span>
+                    <input type="email"required>
+                    <label>correo</label>
+                </div>
+                <div class="input-box">
+                    <span class="icono">
+                        <ion-icon name="lock-closed"></ion-icon>
+                    </span>
+                    <input type="password"required>
+                    <label>contraseña</label>
+                </div>
+                <div class="remember_forgot">
+                    <label><input type="checkbox">acepto terminos y condiciones</label>
+                    <button type="submit" class="btn">
+                        registrarse
+                    </button>
+                    <div class="login-register">
+                        <p>¿Ya tienes una cuenta?
+                            <a href="#"class="login-Link">Iniciar Sesion</a>
+                        </p>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <script defer src="login.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,13 @@
+// esto es para la pagina de folder
+<?php
+    include('session.php');
+?>
+<html>
+<head>
+    <title>Welcome </title>
+</head>
+<body>
+    <h1>Welcome <?php echo $login_session; ?></h1> 
+    <h2><a href = "logout.php">Sign Out</a></h2>
+</body>
+</html>
